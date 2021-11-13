6df7779df67139cb89e9648445928b1e9959a5f3
CelestialArcadia <22876185+CelestialArcadia@users.noreply.github.com>
2021-11-13 02:15:44 +0100

    Eureka server

---
+++ eureka-server/src/main/java/org/project/gallery/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package org.project.gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// Enabling Eureka server
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
